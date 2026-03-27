d70841d776c8f07466bb34a2b8493d49d5186c3f
baldev106 <baldevcrazy23@gmail.com>
2026-03-27 21:21:46 +0530

    Instagram Connect Fixs

--- admin/class-sf-settings.php
+++ admin/class-sf-settings.php
@@ -380,7 +380,8 @@ class SF_Settings {
 		}
 		$state = base64_encode( (string) $page_url );
 
-		$oauth_url = 'https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=2067986510434194&redirect_uri=https://one.mahihub.in/ig-api/oauth.php&response_type=code&scope=instagram_business_basic,instagram_manage_comments,instagram_business_manage_messages&state=' . rawurlencode( $state );
+		$redirect_uri = 'https://one.mahihub.in/ig-api/oauth.php';
+		$oauth_url    = 'https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=2067986510434194&redirect_uri=' . urlencode( $redirect_uri ) . '&response_type=code&scope=instagram_business_basic,instagram_manage_comments,instagram_business_manage_messages&state=' . rawurlencode( $state );
 		?>
 		<a
 			class="sf-connect-instagram-btn sf-oauth-connect-btn"
